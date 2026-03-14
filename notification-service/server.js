b77c9d65883244d21dfb417e5f282d831b3d8726
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 13:06:34 +0530

    Update server.js

--- notification-service/server.js
+++ notification-service/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://orvex-one.vercel.app",
+    origin: "https://orvex.onrender.com",
   },
 });
 
