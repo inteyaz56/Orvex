da4be86dbed9db8afe76a75a52497e50440f41fd
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 14:17:14 +0530

    Update CORS origins to include Vercel app

--- admin-service/app.js
+++ admin-service/app.js
@@ -11,8 +11,11 @@ import adminRoutes from "./routes/admin.routes.js";
 
 app.use(
   cors({
-    origin: "https://orvex.onrender.com",
-  }),
+    origin: [
+      "https://orvex.onrender.com",
+      "https://orvex-one.vercel.app",
+    ],
+  })
 );
 
 app.use(express.json());
