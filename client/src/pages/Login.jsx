7e77f868e62bfdecba354587f5d590d648b45aeb
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 14:29:13 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -29,14 +29,14 @@ const Login = () => {
         email,
         password,
       });
-      console.log(serverUrl);
+ 
       dispatch(setUserData(result.data.user));
       localStorage.setItem("token", result.data.token);
       navigate("/");
     } catch (error) {
       toast.error(error?.response?.data?.message || "Something went wrong");
-       console.log(serverUrl);
-      console.log(error);
+   
+  
     } finally {
       setLoading(false);
     }
