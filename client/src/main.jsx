fded9343a344129a8cbc6f1b8d7f3b96dd3c41ae
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 13:00:56 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "leaflet/dist/leaflet.css";
-export const serverUrl = import.meta.env.VITE_BASE_URL;
+export const serverUrl ="https://orvex-gateway.onrender.com/health";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
