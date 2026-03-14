20f28c408a3fbb884f16ecf29178257c2aca5a7b
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 14:17:47 +0530

    Update app.js

--- notification-service/app.js
+++ notification-service/app.js
@@ -19,7 +19,14 @@ await connectRabbitMQ();
 await startNotificationConsumer();
 
 //Middleware
-app.use(cors({ origin: "https://orvex.onrender.com" }));
+app.use(
+  cors({
+    origin: [
+      "https://orvex.onrender.com",
+      "https://orvex-one.vercel.app",
+    ],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
