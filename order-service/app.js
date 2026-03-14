ba5cdb99575454ba37b887a1524e8b341e843adc
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 12:54:16 +0530

    Update app.js

--- order-service/app.js
+++ order-service/app.js
@@ -22,7 +22,7 @@ await startOrderConsumer();
 //Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://orvex-one.vercel.app",
   }),
 );
 app.use(cookieParser());
