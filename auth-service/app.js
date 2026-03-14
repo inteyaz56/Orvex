2eaeba69f077145caa4b2034eaaa8b94a277204a
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 12:53:28 +0530

    Update CORS origin to production URL

--- auth-service/app.js
+++ auth-service/app.js
@@ -21,7 +21,7 @@ import authRoute from "./routes/auth.routes.js";
 //Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://orvex-one.vercel.app",
   }),
 );
 app.use(cookieParser());
