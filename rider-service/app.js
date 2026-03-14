99c3be0fa98ad545880f3dcf6997a6162829a82d
Inteyaz Ansari <inteyazansari322@gmail.com>
2026-03-14 12:55:33 +0530

    Update CORS origin to production URL

--- rider-service/app.js
+++ rider-service/app.js
@@ -21,7 +21,7 @@ await startRiderConsumer();
 //Miiddlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://orvex-one.vercel.app",
   }),
 );
 app.use(express.json());
